cd12e320aa442706bf0a4f958ed175a6c636b58d
Ryan Bates <ryan@railscasts.com>
2012-12-06 10:27:26 -0800

    refactor Must class a bit

--- lib/musts.rb
+++ lib/musts.rb
@@ -8,4 +8,28 @@ module Musts
   def self.matcher(*args, &block)
     Must.matcher(*args, &block)
   end
+
+  matcher(:equal, :eq) { |other| self == other }
+  matcher(:be_true)    { self }
+  matcher(:be_false)   { !self }
+  matcher(:be_nil)     { nil? }
+  matcher(:include)    { |*args| include?(*args) }
+
+  matcher(:be_greater_than, :be_gt)              { |other| self > other }
+  matcher(:be_greater_than_or_equal_to, :be_gte) { |other| self >= other }
+  matcher(:be_less_than, :be_lt)                 { |other| self < other }
+  matcher(:be_less_than_or_equal_to, :be_lte)    { |other| self <= other }
+
+  matcher(:be) { |method, *args| send(method, *args) }
+
+  matcher(:raise_exception) do |*args|
+    raised = false
+    exception = args.first || StandardError
+    begin
+      call
+    rescue exception
+      raised = true
+    end
+    raised
+  end
 end
--- lib/musts/must.rb
+++ lib/musts/must.rb
@@ -2,7 +2,7 @@ module Musts
   class Must
     def self.matcher(name, *other_names, &block)
       define_method(name) do |*args|
-        result = @target.instance_exec(*args, &block)
+        result = @subject.instance_exec(*args, &block)
         _assert(result, name, args)
       end
       other_names.each do |other_name|
@@ -10,31 +10,9 @@ module Musts
       end
     end
 
-    def initialize(target, negate = false)
-      @target = target
+    def initialize(subject, negate = false)
+      @subject = subject
       @negate = negate
-    end
-
-    matcher(:equal, :eq) { |other| self == other }
-    matcher(:be_greater_than, :be_gt) { |other| self > other }
-    matcher(:be_greater_than_or_equal_to, :be_gte) { |other| self >= other }
-    matcher(:be_less_than, :be_lt) { |other| self < other }
-    matcher(:be_less_than_or_equal_to, :be_lte) { |other| self <= other }
-    matcher(:be) { |method, *args| send(method, *args) }
-    matcher(:be_true) { self }
-    matcher(:be_false) { !self }
-    matcher(:be_nil) { nil? }
-    matcher(:include) { |*args| include?(*args) }
-
-    matcher(:raise_exception) do |*args|
-      raised = false
-      exception = args.first || StandardError
-      begin
-        call
-      rescue exception
-        raised = true
-      end
-      raised
     end
 
   private
@@ -44,7 +22,7 @@ module Musts
         message_action = name.to_s.tr("_", " ")
         message_action = "not " + message_action if @negate
         message_action += " " + args.map(&:inspect).join(", ") unless args.empty?
-        raise Musts::Failure.new("Expected #{@target.inspect} to #{message_action}")
+        raise Musts::Failure.new("Expected #{@subject.inspect} to #{message_action}")
       end
     end
   end
