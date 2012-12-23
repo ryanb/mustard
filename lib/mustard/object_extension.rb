c819387c9d22b81773febb22b09fea652b3d62a4
Garry Shutler <garry@robustsoftware.co.uk>
2012-12-23 16:56:27 +0000

    RSpec subject support
    Created RSpecExtension and included it in the RSpec configuration so that the
    subject becomes the target of Mustard assertions.
    
    Also created mustard/rspec so that using mustard with RSpec is a single require.

--- lib/mustard/object_extension.rb
+++ lib/mustard/object_extension.rb
@@ -8,6 +8,16 @@ module Mustard
       Must.new(self, true)
     end
   end
+
+  module RSpecExtension
+    def must
+      Must.new(subject)
+    end
+
+    def must_not
+      Must.new(subject, true)
+    end
+  end
 end
 
 Object.send(:include, Mustard::ObjectExtension)
